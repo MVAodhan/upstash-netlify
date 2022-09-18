f4aeacd9c334e052585fa32390920016a764903c
MVAodhan <hamilton.aodhan@gmail.com>
2022-09-19 00:26:59 +1200

    ctx headers

--- netlify/edge-functions/hello.js
+++ netlify/edge-functions/hello.js
@@ -3,7 +3,9 @@ import { Redis } from 'https://deno.land/x/upstash_redis@v1.13.1/mod.ts';
 export default async (_req, ctx) => {
   const redis = Redis.fromEnv();
   const links = await redis.hgetall('links');
-  const res = ctx.json(links);
-  res.headers.append('Allow-Access-Control-Origin', 'https://*');
-  return res;
+  return ctx.json(links, {
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+    },
+  });
 };

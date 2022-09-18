2fcf20aa37ed0ec687992f2e85ab31fa4bbb4054
MVAodhan <hamilton.aodhan@gmail.com>
2022-09-18 17:05:14 +1200

    adding a form and getting data back

--- netlify/edge-functions/hello.js
+++ netlify/edge-functions/hello.js
@@ -1,7 +1,7 @@
 import { Redis } from 'https://deno.land/x/upstash_redis@v1.13.1/mod.ts';
 
-export default async () => {
+export default async (req, context) => {
   const redis = Redis.fromEnv();
-  const counter = await redis.incr('edge_counter');
-  return new Response(counter);
+  const links = await redis.hgetall('links');
+  return new Response(JSON.stringify(links));
 };

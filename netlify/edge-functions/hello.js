0c94f4194aedf84a2b90bb5eef74960dc23271e7
MVAodhan <hamilton.aodhan@gmail.com>
2022-09-19 00:20:27 +1200

    add https to cors

--- netlify/edge-functions/hello.js
+++ netlify/edge-functions/hello.js
@@ -1,8 +1,9 @@
 import { Redis } from 'https://deno.land/x/upstash_redis@v1.13.1/mod.ts';
 
-export default async (req, ctx) => {
+export default async (_req, ctx) => {
   const redis = Redis.fromEnv();
   const links = await redis.hgetall('links');
   const res = ctx.json(links);
+  res.headers.append('Allow-Access-Control-Origin', 'https://*');
   return res;
 };
